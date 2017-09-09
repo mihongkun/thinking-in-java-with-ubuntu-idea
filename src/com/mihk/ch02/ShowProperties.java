2553770b986d3bd34c2b5ee89ab6e204529b4492
mihongkun <1045262335@qq.com>
2017-09-09 16:42:36 +0800

    hello world

---
+++ src/com/mihk/ch02/ShowProperties.java
@@ -0,0 +1,22 @@
+package com.mihk.ch02;
+
+import java.util.Date;
+
+public class ShowProperties {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(System.getProperty("java.library.path"));
+        System.out.println("Hello," + new Date());
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+}
