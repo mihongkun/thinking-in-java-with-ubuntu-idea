adfbabbc53d1a7a070ed594bd0436832b840ef60
Jackson Mi <1045262335@qq.com>
2017-09-20 15:30:27 +0800

    add something

--- src/com/mihk/ch10/MultiImplementation.java
+++ src/com/mihk/ch10/MultiImplementation.java
@@ -3,7 +3,22 @@ package com.mihk.ch10;
 class D {}
 abstract class E {}
 
-
+class Z extends D {
+    E makeE(){
+        return new E() {
+        };
+    }
+}
 
 public class MultiImplementation {
+    static void takeD(D d){}
+    static void takeE(E e){}
+
+    public static void main(String[] args) {
+        Z z = new Z();
+        takeD(z);
+        takeE(z.makeE());
+    }
+
+
 }
